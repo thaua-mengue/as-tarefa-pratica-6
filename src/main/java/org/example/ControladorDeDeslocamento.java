564bcacfbace79567a75f5813248139ddea3b843
Thauã Mengue <thaua.mengue@gmail.com>
2024-11-17 19:02:50 -0300

    add observer todo comment

--- src/main/java/org/example/ControladorDeDeslocamento.java
+++ src/main/java/org/example/ControladorDeDeslocamento.java
@@ -2,6 +2,10 @@ package org.example;
 
 // Classe ControladorDeDeslocamento
 class ControladorDeDeslocamento {
+
+    // TODO adicionar observers para enviar notificações para o usuario como chegada ao destino, alterações de rota etc
+
+
     public void iniciarDeslocamento(Rota rota) {
         System.out.println("Deslocamento iniciado!");
         System.out.println(rota.getDetalhes());
